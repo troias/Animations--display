288f2cba249ff35af25504a3a6bbe93a21ae8c4a
Troy Flavell <hunter007nz@gmail.com>
2021-04-01 20:01:57 +1300

    Added React transition groups

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from 'react-transition-group';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -6,17 +7,18 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
-  state = {  
-    modalIsOpen: false
-   }
+  state = {
+    modalIsOpen: false,
+    showBlock: false
+  }
 
-   showModal = () => {
-     this.setState({
-       modalIsOpen: true
-     })
-   }
+  showModal = () => {
+    this.setState({
+      modalIsOpen: true
+    })
+  }
 
-   closeModal = () => {
+  closeModal = () => {
     this.setState({
       modalIsOpen: false
     })
@@ -26,9 +28,30 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal closed={this.closeModal} show={this.state.modalIsOpen}/> 
-         <Backdrop show={this.state.modalIsOpen}/> 
-        <button  className="Button" onClick={this.showModal}>Open Modal</button>
+        <button className="Button" onClick={() => this.setState((prevState) => ({
+          showBlock: !prevState.showBlock
+        }))}> Toggle  </button>
+        <br />
+        <Transition in={this.state.showBlock} 
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit>
+          {state => ( <div style={{
+            backgroundColor: 'red',
+            width: 100,
+            height: 100,
+            paddingBottom: '20px',
+            margin: 'auto',
+            transition: 'opacity 1s easy-out',
+            opacity: state === 'exiting' ? 0 : 1,
+          }}></div>)} 
+
+        </Transition>
+
+
+        {  this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        {  this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
